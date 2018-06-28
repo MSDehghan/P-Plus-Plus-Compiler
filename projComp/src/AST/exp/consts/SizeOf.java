f1bcdfa8dadcf63211748ef427898b97f348b70c
MSDehghan <msdn2013@gmail.com>
2018-06-29 03:12:01 +0430

    Fix Some Mistakes

--- projComp/src/AST/exp/consts/SizeOf.java
+++ projComp/src/AST/exp/consts/SizeOf.java
@@ -3,15 +3,23 @@ package AST.exp.consts;
 import AST.SymbolTable.SymbolTable;
 import AST.exp.Exp;
 import jdk.internal.org.objectweb.asm.MethodVisitor;
+import jdk.internal.org.objectweb.asm.Type;
 
 /**
  * Created by pooria on 6/29/2018.
  */
-public class SizeOf extends Exp{
-    Integer value;
-    public SizeOf(String id){
+public class SizeOf extends Exp {
+    private Integer value;
+
+    public SizeOf(String id) {
+        type = Type.INT_TYPE;
         this.value = SymbolTable.getSize(id);
     }
+
+    public Integer getValue() {
+        return value;
+    }
+
     @Override
     public void compile(MethodVisitor mv) {
 
