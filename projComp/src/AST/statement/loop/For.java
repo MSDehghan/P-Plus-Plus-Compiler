9cd1dedf582ed3eb8e02eae2eacd880a5a19d7de
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2018-06-28 17:28:50 +0430

    parser fixed

--- projComp/src/AST/statement/loop/For.java
+++ projComp/src/AST/statement/loop/For.java
@@ -1,14 +1,19 @@
 package AST.statement.loop;
 
 import AST.block.Block;
+import AST.declaration.VariableDeclaration;
+import AST.exp.Exp;
 import jdk.internal.org.objectweb.asm.MethodVisitor;
 
 /**
  * Created by pooria on 6/28/2018.
  */
 public class For extends Loop {
-    For(Block block){
+    VariableDeclaration variableDeclaration ;
+    Exp exp;
+    For(Block block, Exp exp){
         super(block);
+        this.exp = exp ;
     }
     @Override
     public void compile(MethodVisitor mv) {
